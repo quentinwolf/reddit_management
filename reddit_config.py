abe3dad45963af7c122e692f4a23b230538bd2df
quentinwolf <18410863+quentinwolf@users.noreply.github.com>
2024-07-04 14:01:09 -0600

    Create reddit_config.py

---
+++ reddit_config.py
@@ -0,0 +1,12 @@
+# Imports Don't edit:
+import praw
+
+# Configuration Settings:
+
+# This should match the title in praw.ini or if you have multiple entries in praw.ini pick which one you want to use here.
+reddit = praw.Reddit("reddit_login") 
+
+# Your Moderator Username without the u/
+moderator_name = "yourmoderatorname"
+
+backup_directory = "Backups"
